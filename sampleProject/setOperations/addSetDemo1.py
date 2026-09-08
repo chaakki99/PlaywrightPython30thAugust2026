81dd062dabf4f35ddc179cd0c1ae7b82e34168dc
chaakki99 <chaakki99@gmail.com>
2026-09-08 20:28:47 +0530

    List Tuple Set Updates

---
+++ sampleProject/setOperations/addSetDemo1.py
@@ -0,0 +1,30 @@
+#Case 3: How to Add Elements in to a Set
+#Solution:
+# Case 3: How to Add Elements in to a Set
+# Declare a Set
+cities = {"Mangalore"}
+# Add Elements in to a Set
+cities.add("Mysore")
+cities.add("Bangalore")
+cities.add("Tumkur")
+cities.add("Kolar")
+cities.add("Bellary")
+# print Elements from set
+print(cities)
+
+
+===========================================================================
+
+Case 3: How to declare blank Set and Add Elements.
+# Case 3: How to Add Elements in to a Set
+# Declare a Set
+cities = {""}
+ 
+# Add Elements in to a Set
+cities.add("Mysore")
+cities.add("Bangalore")
+cities.add("Tumkur")
+cities.add("Kolar")
+cities.add("Bellary")
+# print Elements from set
+print(cities)
---
+++ sampleProject/setOperations/setDemo1.py
@@ -0,0 +1,6 @@
+#Case 1: How to declare a set and How to Add Elements in to a set.
+#Solution:
+# Declare a Set
+fruits = {"Mango","Apple","Banana","Mango","Grapes","Watermelon","Banana"}
+# print elements from set
+print(fruits)
